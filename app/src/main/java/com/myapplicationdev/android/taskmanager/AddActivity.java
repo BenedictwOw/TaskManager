52030d08b08eae84feca8ac37d90c934ca008136
18044928 <18044928@myrp.edu.sg>
2020-05-28 14:43:29 +0800

    finalps

--- app/src/main/java/com/myapplicationdev/android/taskmanager/AddActivity.java
+++ app/src/main/java/com/myapplicationdev/android/taskmanager/AddActivity.java
@@ -1,37 +1,64 @@
 package com.myapplicationdev.android.taskmanager;
 
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddActivity extends AppCompatActivity {
-    EditText etName,etDescription;
+    int reqCode = 12345;
+    EditText etName,etDescription,etTime;
     Button btnAdd,btnCancel;
     String name,des;
+    Integer time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
         etName =(EditText)findViewById(R.id.etName);
         etDescription = (EditText)findViewById(R.id.etDescription);
+        etTime = (EditText)findViewById(R.id.etTime);
+
         btnAdd = (Button)findViewById(R.id.btnAdd);
         btnCancel = (Button)findViewById(R.id.btnCancel);
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                time = Integer.parseInt(etTime.getText().toString());
+                cal.add(Calendar.SECOND, time);
                 DBHelper dbh = new DBHelper(AddActivity.this);
                 name = etName.getText().toString().trim();
                 des = etDescription.getText().toString().trim();
+
                 dbh.insertTasks(name,des);
                 dbh.close();
-                Intent i = new Intent(AddActivity.this, MainActivity.class);
-                startActivity(i);
+                Intent intent = new Intent(AddActivity.this,
+                        ScheduledNotificationReceiever.class);
+                intent.putExtra("data",name);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                        AddActivity.this, reqCode,
+                        intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager am = (AlarmManager)
+                        getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                        pendingIntent);
+
+
             }
         });
         btnCancel.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/myapplicationdev/android/taskmanager/ScheduledNotificationReceiever.java
+++ app/src/main/java/com/myapplicationdev/android/taskmanager/ScheduledNotificationReceiever.java
@@ -1,15 +1,64 @@
 package com.myapplicationdev.android.taskmanager;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+import static android.content.Context.NOTIFICATION_SERVICE;
 
 public class ScheduledNotificationReceiever extends BroadcastReceiver {
+    int reqCode = 12345;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new
+                    NotificationChannel("default", "Default Channel",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("This is for default notification");
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Intent i= new Intent(context, AddActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity
+                (context, reqCode, i,
+                        PendingIntent.FLAG_CANCEL_CURRENT);
+        Uri uri= RingtoneManager.getDefaultUri
+                (RingtoneManager.TYPE_ALARM);
+
+
+        Bitmap picture = BitmapFactory.decodeResource(context.getResources(), R.drawable.download);
+
+        // Build notification
+        NotificationCompat.Builder builder = new
+                NotificationCompat.Builder(context, "default");
+        builder.setContentTitle("Task Manager Reminder");
+        builder.setContentText(intent.getStringExtra("data"));
+        builder.setSmallIcon(android.R.drawable.ic_dialog_info);
+        builder.setLargeIcon(picture);
+        builder .setStyle(new NotificationCompat.BigPictureStyle()
+                .bigPicture(picture)
+                .bigLargeIcon(null));
+        builder.setContentIntent(pIntent);
+        builder.setAutoCancel(true);
+        builder.setSound(uri);
+
+        Notification n = builder.build();
+        notificationManager.notify(123, n);
     }
+
 }
